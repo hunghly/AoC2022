57960bf116fb452ca6edca42074c7bf60a42c71c
hunghly <hunghly@gmail.com>
2022-12-06 15:54:52 -0600

    need to work on 2

---
+++ 2/main1.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+// AOC Day 2 Part 1
+int main() {
+  ifstream file{"input1.txt"};
+  string line;
+  char opponent {0};
+  char response {0};
+  while (getline(file, line)) {
+    cout << "Opponent:" << endl;
+  }
+
+}
