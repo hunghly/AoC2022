d810d00c3f531edadae245f216e5bf00f48e6baf
hunghly <hunghly@gmail.com>
2022-12-07 16:15:23 -0600

    first day 6 part 1

---
+++ 6/main1.cpp
@@ -0,0 +1,54 @@
+/** COMPILE WITH C++ 20
+***/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <iomanip>
+
+using namespace std;
+
+bool verifyDuplicates(string buffer) {
+    set<char> s {0};
+    for (char ch : buffer) {
+        if (s.contains(ch)) {
+            return false;
+        }
+        s.insert(ch);
+    }
+    return true;
+}
+
+void processStream(string line) {
+    int counter {0};
+    string buffer;
+    for (char ch : line) {
+        buffer += ch;
+        counter++;
+        // every four characters, perform a check
+        if (buffer.size() == 4) {
+            // If no duplicates, then it's the start of the packet
+            if (verifyDuplicates(buffer)) {
+                cout << "Answer: " << counter << endl;
+            } else {
+                // remove the first character
+                buffer.erase(0,1);
+            }
+        }
+    }
+
+}
+
+
+//AOC Day 6 Part 1
+int main() {
+    ifstream file {"input1.txt"};
+    string line;
+    while (getline(file, line)) {
+        processStream(line);
+    }
+}
