9468ba4480d39ca1293c1481d1462060e044c3e2
Hung Ly <hunghly@gmail.com>
2022-12-08 13:11:09 -0600

    working on 7

--- 7/main1.cpp
+++ 7/main1.cpp
@@ -3,75 +3,171 @@
 
 #include <iostream>
 #include <fstream>
+#include <istream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include <set>
 #include <iterator>
 #include <iomanip>
 #include <list>
+#include <memory>
 #include "main1.h"
 
 using namespace std;
 
-list<Directory> directories {};
-string currentDirectory {};
+list<Directory*> directories;
+// string currentDirectory {};
+Directory *currDirectoryPtr = NULL;
 Directory emptyDir {""};
 
-bool isACommand(string line) {
-    // Check if command or std output
-    if (line[0] == '$') {
-        cout << "Parsing command:" << line << endl;
-        return true;
+void printDirectory(Directory* directory, int distance) {
+    for (int i = 0; i < directory->distanceFromRoot; i++) {
+        cout << "\t";
     }
-    return false;
+    cout << "(dir) " << directory->name << "(" << directory->dirFileSize << ")";
+    if (directory->subdirectories.size() > 0) {
+        cout << "[" << directory->dirFileSize + directory->getSubDirFileSize() << "]";
+    }
+     cout << endl;
+
+    if (directory->files.size() > 0) {
+        for (auto f : directory->files) {
+            for (int i = 0; i < directory->distanceFromRoot; i++) {
+            cout << "\t";
+            }
+            cout << "\t(file) " << f.name << " (" << f.size << ")" << endl;
+        }
+    }
 }
 
 void printDirectories() {
-    for (auto directory : directories) {
-        cout << "Directory name: " << directory.name << endl;
-        if (directory.name != "root") {
-            cout << "Directory parent: " << directory.parent->name << endl;
+    cout << "===Printing Dirs===" << endl;
+    list<Directory*> tmp = directories;
+    for (auto &directory : tmp) {
+        cout << "directory: " << directory->name << directory->subdirectories.size() << endl;
+        if (directory->subdirectories.size() > 0) {
+            // while (directory->subdirectories.size() > 0) {
+            //     for (auto subdir : directory->subdirectories) {
+            //         cout << "printing subdirs?" << endl;
+            //         printDirectory(subdir, subdir->distanceFromRoot);
+            //     }
+            //     cout << "removing: " << (*directory->subdirectories.begin())->name << endl;
+            //     directory->subdirectories.erase(directory->subdirectories.begin());
+            // }
+
+        } else {
+            cout << "calling else " << endl;
+            printDirectory(directory, directory->distanceFromRoot);
+            // tmp.erase(tmp.begin());
         }
+
+        
+
+
+
+        // cout << "DFR: " << directory->name << directory->distanceFromRoot << endl;
+        // for (int i = 0; i < directory->distanceFromRoot; i++) {
+        //     cout << "\t";
+        // }
+        // cout << "(dir) " << directory->name << "(" << directory->dirFileSize << ")";
+        // if (directory->subdirectories.size() > 0) {
+        //     cout << "[" << directory->dirFileSize + directory->getSubDirFileSize() << "]";
+        // }
+        // cout << endl;
+
+        // if (directory->files.size() > 0) {
+        //     for (auto f : directory->files) {
+        //         for (int i = 0; i < directory->distanceFromRoot; i++) {
+        //         cout << "\t";
+        //         }
+        //         cout << "\t(file) " << f.name << " (" << f.size << ")" << endl;
+        //     }
+        // }
+        // cout << endl;
+
+
+
+
+        // if (directory->name != "root") {
+        //     cout << "Parent: " << directory->parent->name << "| ";
+        // }
+        // if (directory->subdirectories.size() > 0) {
+        //     cout << "Subdirectories: ";
+        //     for (Directory* subdir : directory->subdirectories) {
+        //         cout << subdir->name << ", ";
+        //     }
+        //     cout << "Subdir total filesize: " << directory->getSubDirFileSize() << "| ";
+        // }
+        // if (directory->files.size() > 0) {
+        //     cout << "Files: ";
+        //     for (File file : directory->files) {
+        //         cout << file.name << "[" << file.size << "]" << ", ";
+        //     }
+        //     cout << "| Directory File Size: " << directory->dirFileSize;
+        // }
+        // cout << " Total File Size: " << directory->getSubDirFileSize() + directory->dirFileSize;
+        // cout << endl;
+        // cout << "Directory name: " << directory->name << "| ";
+        // if (directory->name != "root") {
+        //     cout << "Parent: " << directory->parent->name << "| ";
+        // }
+        // if (directory->subdirectories.size() > 0) {
+        //     cout << "Subdirectories: ";
+        //     for (Directory* subdir : directory->subdirectories) {
+        //         cout << subdir->name << ", ";
+        //     }
+        //     cout << "Subdir total filesize: " << directory->getSubDirFileSize() << "| ";
+        // }
+        // if (directory->files.size() > 0) {
+        //     cout << "Files: ";
+        //     for (File file : directory->files) {
+        //         cout << file.name << "[" << file.size << "]" << ", ";
+        //     }
+        //     cout << "| Directory File Size: " << directory->dirFileSize;
+        // }
+        // cout << " Total File Size: " << directory->getSubDirFileSize() + directory->dirFileSize;
+        // cout << endl;
     }
 }
 
-Directory& getDirectory(string name) {
+
+Directory* getDirectory(string name) {
     for (auto &directory : directories) {
-        if (directory.name == name) {
-            cout << "Directory exists:" << name << endl;
+        if (directory->name == name) {
+            // cout << "Directory exists:" << name << endl;
             return directory;
         }
     }
-    return emptyDir;
+    return &emptyDir;
 }
 
 void runCD(vector<string> command) {
-    // if (command[1] == "cd") {
-        if (command[2] == "/") {
-            if (directories.size() == 0) {
-                // create root directory
-                directories.push_back(Directory {"root", NULL});
-            }
-            currentDirectory = "root";
+    if (command[2] == "/") {
+        if (directories.size() == 0) {
+            // create root directory
+            directories.push_back(new Directory {"root", NULL});
         }
-        else if (command[2] == "..") {
-            // implement this
-        }
+        currDirectoryPtr = getDirectory("root");
+    }
+    else if (command[2] == "..") {
+        // change directory up one level
+        currDirectoryPtr = currDirectoryPtr->parent;
+        cout << "went up one level to: " << currDirectoryPtr->name << endl;
+    }
+    else {
+        // if directory doesn't exist in list, add it
+        if (getDirectory(command[2]) == &emptyDir) {
+            // cout << "Creating new directory!" << endl;
+            Directory *parent = getDirectory(currDirectoryPtr->name);
+            directories.push_back(new Directory {command[2], parent});
+            currDirectoryPtr = getDirectory(command[2]);
+        } 
+        // just change directory
         else {
-            // if directory doesn't exist in list, add it
-            if (getDirectory(command[2]) == emptyDir) {
-                cout << "Creating new directory!" << endl;
-                Directory *parent = &getDirectory(currentDirectory);
-                directories.push_back(Directory {command[2], parent});
-                currentDirectory = command[2];
-            } 
-            // just change directory
-            else {
-                currentDirectory = command[2];
-            }
-        }     
-    // }
+            // cout << "Changing to directory: " << command[2] << endl;
+            currDirectoryPtr = getDirectory(command[2]);
+        }
+    }
 }
 
 void printCommand(vector<string> command) {
@@ -83,6 +179,83 @@ void printCommand(vector<string> command) {
     cout << endl;
 }
 
+// Get the data that follows an ls command
+vector<string> getLSData(ifstream &file) {
+    // cout << "Getting LS data." << endl;
+    vector<string> data {};
+    string line;
+    while (getline(file, line)) {
+        data.push_back(line);
+        // Check if the next line contains a command.
+        if ((char) file.peek() == '$')
+            return data;
+    }
+    return data;
+}
+
+// parse the ls data
+void parseLSData(vector<string> data) {
+    // cout << "Parsing LS Data for Current Directory:" << currDirectoryPtr->name << endl;
+    for (string line : data) {
+        // two conditions: 1. listing directory 2. listing file details
+
+        // Condition 1
+        if (line.substr(0, line.find(" ")) == "dir") {
+            string directoryName = line.substr(line.find(" ")+1);
+            if (getDirectory(directoryName) == &emptyDir) {
+                // cout << "Found a new directory using LS! Current directory: " << currDirectoryPtr->name << endl;
+                // Directory* newDir = new Directory(directoryName, currDirectoryPtr);
+                directories.push_back(new Directory(directoryName, currDirectoryPtr));
+                currDirectoryPtr->subdirectories.push_back(directories.back());
+
+
+                // Directory *parent = getDirectory(currDirectoryPtr->name);
+                // cout << parent->name << endl;
+                // parent->subdirectories->push_back(directoryName);
+                // parent->subdirectories.
+            }
+        }
+        // Condition 2
+        else {
+            // int fileSize = stoi(line.substr(0, line.find(" ")));
+            // string filename = line.substr(line.find(" ") + 1);
+            // cout << "file size:" << fileSize << " filename:" << filename << endl;
+            File newFile;
+            newFile.size = stoi(line.substr(0, line.find(" ")));
+            newFile.name = line.substr(line.find(" ") + 1);
+            currDirectoryPtr->files.push_back(newFile);
+            currDirectoryPtr->updateDirFileSize();
+        }
+    }
+}
+
+// int getRecursiveFileSize(Directory* d, int sum) {
+//     cout << "sum is: " << sum << endl;
+
+//     // lowest level dir
+//     if (d->dirFileSize == 0 || d->parent == NULL) {
+//         return sum;
+//     }
+//     sum += d->dirFileSize;
+//     // else if it's not, then get the current filesize + parents
+//     return getRecursiveFileSize(d->parent, sum);
+// }
+
+int findDirectoriesSum(int maxSize) {
+    int sum = 0;
+    vector<Directory> foundDirectories {};
+    for (Directory* d : directories) {
+        if ((d->dirFileSize + d->getSubDirFileSize()) < maxSize) {
+            cout << "Checking directory: " << d->name << " Total Size:" <<  d->dirFileSize + d->getSubDirFileSize() << endl;
+            sum+= (d->dirFileSize +  d->getSubDirFileSize());
+            
+        }
+    }
+    return sum;
+}
+
+
+
 //AOC Day 7 Part 1
 int main() {
     ifstream file {"input1.txt"};
@@ -90,28 +263,40 @@ int main() {
     Directory root {"root"};
     string token {};
     vector<string> command {};
+    vector<string> lsData {};
+    int directorySum {};
     while (getline(file, line)) {
+        cout << "Running:" << line << endl;
         // Perform command
-        if(isACommand(line)) {
+        if(line[0] == '$') {
             stringstream ss(line);
             while(getline(ss, token, ' ')) {
                 if (token != "\0") {
-                    // cout << "tok" << token << endl
                     command.push_back(token);
                 }
             }
-            printCommand(command);
             if (command[1] == "cd") {
                 runCD(command);
             } else if (command[1] == "ls") {
                 // get lines until next command detected
+                lsData = getLSData(file);
+                parseLSData(lsData);
+                lsData.clear();
             }
-            printDirectories();
             // clear command after running
             if (!command.empty()) {
                 // cout << "Clearing" << endl;
                 command.clear();
             }
+            printDirectories();
         };
     }
+    
+    directorySum =  findDirectoriesSum(100000);
+    cout << "Answer: " << directorySum << endl;
+    // Delete pointers
+    // for (auto &dir : directories) {
+        // delete dir->subdirectories;
+        // delete dir.parent;
+    // }
 }
--- 7/main1.h
+++ 7/main1.h
@@ -1,29 +1,76 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <ratio>
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 struct File {
     string name;
-    string size;
+    int size;
 };
 
 class Directory {
 public:
     string name;
     Directory *parent;
+    vector<Directory*> subdirectories;
     vector<File> files;
+    int distanceFromRoot = 0;
+    int dirFileSize = 0;
+    
+
     Directory(string name) {
         this->name = name;
     }
     Directory(string name, Directory *parent) {
         this->name = name;
         this->parent = parent;
+        // this->subdirectories = NULL;
+        if (this->parent != NULL) {
+            Directory* tmp = parent;
+            int distance = 1;
+            while (tmp->parent != NULL) {
+                cout << "finding distance: " << tmp->parent->name;
+                distance++;
+                tmp = tmp->parent;
+            }
+            cout << "Distance from Root: " << distance << endl;
+            distanceFromRoot = distance;
+        }
+        // if (tmp->parent != NULL) {
+        //     cout << "finding distance: " << tmp.parent->name;
+        //     tmp = *tmp.parent;
+        // }
+    }
+    Directory(string name, Directory *parent, vector<Directory*> subdirectories) {
+        this->name = name;
+        this->parent = parent;
+        this->subdirectories = subdirectories;
+    }
+    ~Directory() {
+        cout << "inside deconstructor" << endl;
     }
     bool operator==(const Directory &rhs) const { return this->name == rhs.name;};
+    
+    void updateDirFileSize() {
+        int sum = 0;
+        for (File f : files) {
+            sum += f.size;
+        }
+        dirFileSize = sum;
+    }
+
+    int getSubDirFileSize() {
+        int sum = 0;
+        for (Directory* d : subdirectories) {
+            sum += d->dirFileSize;
+        }
+        return sum;
+    }
 };
 
 
